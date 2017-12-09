a6828cdae85a7d27a064832a12a44ef7e9d95c9d
Dara Hoy <darahoy@gmail.com>
2017-12-09 16:57:30 +0000

    Add dynamic geolocation to successful api call to openweather.org

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 /*global $ APIKEY navigator*/
+
+//Variables
+var lat, long;
+var tempUnit = 'C';
+var x = navigator.geolocation;
+
 $(document).ready(function() {
-    //Variables
-    var lat = document.getElementById("latitude");
-    var long = document.getElementById("longitude");
-    var x = navigator.geolocation;
-
-    x.getCurrentPosition(success, failure);
-
-    function success(position) {
-        var myLat = position.coords.latitude;
-        var myLong = position.coords.longitude;
-        $('#lat').html(myLat);
-        $('#long').html(myLong);
+    if (navigator.geolocation) {
+        x.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            $('#lat').html(lat);
+            $('#long').html(lon);
+            getWeather(lat, lon);
+        });
     }
-
-    function failure() {
-        $('#lat').html("<p> Position coordinates  not available </p> ")
+    else {
+        $('#lat').html("<p> Position coordinates not available </p> ")
     }
-
-
-    //Geoloaction to gather lat, long, city data
-
-
-    function showPosition(position) {
-        lat.value = position.coords.latitude;
-        long.value = position.coords.longitude;
-    }
-
-
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.openweathermap.org/data/2.5/weather',
-        data: {
-            zip: '02907',
-            apiKey: APIKEY
-        },
-        success: function(data) {
-            console.log(data);
-        },
-        fail: function() {
-            console.log(500)
-        }
-    })
 
 })
+
+function getWeather(lat, lon) {
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather",
+        data: {
+            lat: lat,
+            lon: lon,
+            apiKey: APIKEY
+        },
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert(textStatus, errorThrown)
+        }
+    })
+};
