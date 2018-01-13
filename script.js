36dc00b38093cae85c4ee1b87e3d430728219ede
Dara Hoy <darahoy@gmail.com>
2018-01-13 14:17:59 +0000

    Add unit conversion to API

--- script.js
+++ script.js
@@ -2,50 +2,87 @@
 
 //Variables
 var lat, lon;
-var unit = $("#unitSelect").val();
 var x = navigator.geolocation;
 
-//
+// Get weather based on location
 $(document).ready(function() {
-    if (navigator.geolocation) {
+    if (x) {
         x.getCurrentPosition(function(position) {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            $('#lat').html(lat);
-            $('#long').html(lon);
-            getWeather(lat, lon, unit);
+            weather.geoLocation(lat, lon);
         });
     }
-    else {
-        $('#lat').html("<p> Position coordinates not available </p> ")
+    else { //alert to input zip
+        alert('input zip')
     }
-
 })
 
-function getWeather(lat, lon, unit) {
-    $.ajax({
-        method: "GET",
-        url: "https://api.openweathermap.org/data/2.5/weather",
-        data: {
-            lat: lat,
-            lon: lon,
-            units: "imperial",
-            apiKey: APIKEY
-        },
-        success: function(response) {
-            console.log(response);
-            $('#city').html(response.name);
-            $('#country').html(response.sys.country);
-            $('#temp').html(Math.round(response.main.temp));
-            $('#type').html(`${response.weather[0].main}`);
-            // getIcon();
-            // setTempColor();
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert(textStatus, errorThrown)
+var unit = {
+    measure: 'imperial',
+    symbol: '&#8457',
+    change: function() {
+        if (this.measure === 'imperial') {
+            this.measure = 'metric';
+            this.symbol = '&#8451';
         }
-    })
-};
+        else {
+            this.measure = 'imperial';
+            this.symbol = '&#8457';
+        }
+        weather.geoLocation(lat, lon)
+    }
+}
+
+var weather = {
+    geoLocation: function(lat, lon) {
+        $.ajax({
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            data: {
+                lat: lat,
+                lon: lon,
+                units: unit.measure,
+                apiKey: APIKEY
+            },
+            success: function(response) {
+                console.log(response);
+                $('#city').html(response.name);
+                $('#country').html(response.sys.country);
+                $('#temp').html(Math.round(response.main.temp) + `${unit.symbol}`);
+                // getIcon();
+                // setTempColor();
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert(textStatus, errorThrown)
+            }
+        })
+    },
+    zip: function(zip) {
+        $.ajax({
+            method: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            data: {
+                zip: zip,
+                units: unit.measure,
+                apiKey: APIKEY
+            },
+            success: function(response) {
+                console.log(response);
+                $('#city').html(response.name);
+                $('#country').html(response.sys.country);
+                $('#temp').html(Math.round(response.main.temp));
+                // getIcon();
+                // setTempColor();
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert(textStatus, errorThrown)
+            }
+        })
+    }
+}
+
+
 
 // getWeather by zip
 // temp conversion api call
